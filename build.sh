66a946a523398cb8449ce025e84454b1fe41777f
Thang <lhuthng@gmail.com>
2024-01-31 01:48:47 +0100

    add generator

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@ mkdir -p build
 cd build
 
 debug_flag=0
-copy_flag=0
+generate_flag=0
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -d)
             debug_flag=1
             ;;
-        -c)
-            copy_flag=1
+        -g)
+            generate_flag=1
             ;;
         *)
             echo "Unknown option: $1"
@@ -23,13 +23,13 @@ done
 if [ "$debug_flag" == "1" ]; then
     echo "DEBUG MODE enabled"
     cmake -DCMAKE_BUILD_TYPE=DebugMode ..
+elif [ "$generate_flag" == "1" ]; then
+    echo "GENERATE MODE enabled"
+    cmake -DCMAKE_BUILD_TYPE=GenerateMode ..
 else
     echo "DEBUG MODE disabled"
     cmake -DCMAKE_BUILD_TYPE=Release ..
 fi
 
 cmake --build .
-if [ "$copy_flag" == "1" ]; then
-    mv mincrossing ../mincrossing
-fi
 cd ..
